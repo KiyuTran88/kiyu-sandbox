d5ff4733453055542d6f76047a9c0172ebc654d6
KiyuTran88 <79184343+KiyuTran88@users.noreply.github.com>
2021-04-18 21:26:50 +0800

    updated Shopping.vue
    - add item on top of array (unshift)
    - remove item ( splice )
    - find item ( use array.index to find index of item in array )

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,7 @@ const routes = [
   {
     path: '/shopping',
     name: 'Shopping',
-    component: () => import('../views/ShoppingCart.vue')
+    component: () => import('../views/Shopping.vue')
   },
   {
     path: '/grocery',
