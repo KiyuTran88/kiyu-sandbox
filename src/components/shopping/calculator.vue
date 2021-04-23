2e26372fc7d62a1591decd09a4dbb3cf4385b288
KiyuTran88 <79184343+KiyuTran88@users.noreply.github.com>
2021-04-23 15:50:46 +0800

    updated total amount in Food vue

--- src/components/shopping/calculator.vue
+++ src/components/shopping/calculator.vue
@@ -6,9 +6,11 @@
 				<div class="label">${{ item.price }}</div>
 			</div>
 			<div class="button-wrap">
-				<div class="button-containted" v-if="item.amount > 0" @click="$emit('updateAmount','minus', item.label)">-</div>
+				<div class="button-containted" v-if="item.amount > 0" @click="$emit('updateAmount', 'minus', item.label)">
+					-
+				</div>
 				<div class="amount-item">{{ item.amount }}</div>
-				<div class="button-containted" @click="$emit('updateAmount','add', item.label)">+</div>
+				<div class="button-containted" @click="$emit('updateAmount', 'add', item.label)">+</div>
 			</div>
 		</div>
 	</div>
@@ -16,7 +18,7 @@
 		<div class="total">
 			<div>Total:</div>
 			<div style="font-size: 1.1rem">
-				<div>$</div>
+				<div>{{ calculateTotal }}$</div>
 			</div>
 		</div>
 	</div>
@@ -24,24 +26,31 @@
 
 <script>
 export default {
-   name:"calculator",
-   data (){
-      return{}
-   },
-	methods:{
-
+	name: "calculator",
+	data() {
+		return {}
 	},
-   props: {
-      itemList: Object,
-   },
-   emits: ["updateAmount","getIndex"]
+	computed: {
+		calculateTotal() {
+			let totalPrice = 0
+			for (const item of this.itemList) {
+				console.log(item.amount * item.price)
+				totalPrice = totalPrice + item.amount * item.price
+			}
+			return totalPrice
+		},
+	},
+	props: {
+		itemList: Object,
+	},
+	emits: ["updateAmount", "getIndex"],
 }
 </script>
 
 <style lang="scss" scroped>
 @import url("https://fonts.googleapis.com/css2?family=Mulish:wght@200;400;600;800&display=swap");
-.main-wrap{
-	.food-content{
+.main-wrap {
+	.food-content {
 		margin: 0.25rem 0;
 	}
 }
